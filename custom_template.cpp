645f0a26b807b171e0e537212275d77b07440d12
node-overflow <piyushjha.exe@gmail.com>
2025-06-25 19:10:07 +0530

    Create custom_template.cpp

---
+++ custom_template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define pb push_back
+ 
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   
+   ll t;
+   cin >> t;
+   while (t--) {
+        
+   }
+}
