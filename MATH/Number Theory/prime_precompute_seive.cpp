c6b9fd130f8420bbd8989e2a8dcd65002f161723
Piyush <piyushjha.exe@gmail.com>
2025-08-11 19:45:55 +0530

    Create prime_precompute_seive.cpp

---
+++ MATH/Number Theory/prime_precompute_seive.cpp
@@ -0,0 +1,12 @@
+vector<ll> primes;
+
+void sieve(int limit) {
+    vector<bool> isPrime(limit + 1, true);
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= limit; ++i) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= limit; j += i) isPrime[j] = false;
+        }
+    }
+    for (int i = 2; i <= limit; ++i) if (isPrime[i]) primes.push_back(i);
+}
