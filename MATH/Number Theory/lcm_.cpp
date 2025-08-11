8d91a832fe679fe1e5a16ddbf3f1c724c4086932
Piyush <piyushjha.exe@gmail.com>
2025-08-11 20:27:29 +0530

    Create lcm_.cpp

---
+++ MATH/Number Theory/lcm_.cpp
@@ -0,0 +1,12 @@
+ll gcd(ll a, ll b) {
+    while (b != 0) {
+        ll temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    return (a / gcd(a, b)) * b;
+}
