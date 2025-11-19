62070e95371674cb547bf70aad7680695be181ba
Piyush <piyushjha.exe@gmail.com>
2025-11-19 20:32:07 +0530

    Update custom_template.cpp

--- custom_template.cpp
+++ custom_template.cpp
@@ -1,8 +1,9 @@
 // code written by PIYUSH JHA - @node_overflow
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-using ld = long double;
+#define ll long long
+#define lld long double
+#define ull unsigned long long
 #define fio ios::sync_with_stdio(false); cin.tie(nullptr);
 #define endl "\n"
 #define pb push_back
@@ -10,19 +11,22 @@ using ld = long double;
 #define ss second
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)(x).size()
+/* For printing output */
 #define cont(x) {cout << x << "\n"; continue;}
 #define cont2(x, y) {cout << x << " " << y << "\n"; continue;}
+#define cont3(x, y, z) {cout << x << " " << y << " " << z << "\n"; continue;}
 #define printv(v) for (const auto &it : v) cout << it << " "; cout << "\n"
+/* For Debugging */
 #define print(x) cout << x << "\n";
 #define print2(x, y) cout << x << " " << y << "\n";
 #define print3(x, y, z) cout << x << " " << y << " " << z << "\n";
- 
+
 int main() {
     fio;
     
     int tt = 1;
     cin >> tt;
     while (tt--) {
-        
+       
     }
 }
