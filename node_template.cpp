d691b7770d15b725c07e67870a74853673aca4b1
Piyush <piyushjha.exe@gmail.com>
2026-01-16 15:27:03 +0530

    Update node_template.cpp

--- node_template.cpp
+++ node_template.cpp
@@ -2,23 +2,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+#define cfio freopen ("input.txt", "r", stdin); freopen ("output.txt", "w", stdout);
 #define fio ios::sync_with_stdio(false); cin.tie(nullptr);
-#define endl "\n"
+const char nl = '\n';
 #define pb push_back
 #define ff first
 #define ss second
+#define YES "YES"
+#define NO "NO"
 #define all(x) x.begin(), x.end()
 #define sz(x) (int)(x).size()
-#define cont(x) {cout << x << "\n"; continue;}
-#define cont2(x, y) {cout << x << " " << y << "\n"; continue;}
+#define minv(x) *min_element(all(x))
+#define maxv(x) *max_element(all(x))
+/* For printing output */
+#define ret(x) {cout << x << "\n"; return;}
 #define printv(v) for (const auto &it : v) cout << it << " "; cout << "\n"
+/* For Debugging */
+#define print(x) cout << x << "\n";
+#define print2(x, y) cout << x << " " << y << "\n";
+#define print3(x, y, z) cout << x << " " << y << " " << z << "\n";
+/* Helper functions */
+auto is_substr = [](const string& a, const string& b) {return a.find(b) != string::npos;};
+
+
+
+
+
+/* -------------------------- CODE STARTS HERE -------------------------- */
+
+void solve() {
+    
+}
 
 int main() {
+    cfio;
     fio;
     
     int tt = 1;
-    cin >> tt;
+    // cin >> tt;
     while (tt--) {
-        
+        solve();
     }
+    return 0;
 }
