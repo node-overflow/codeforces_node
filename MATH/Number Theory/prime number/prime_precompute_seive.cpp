c3b618fb5fc7ecb4f61ca9ad9461d0c991d2ad58
Piyush <piyushjha.exe@gmail.com>
2026-02-12 19:05:11 +0530

    Update prime_precompute_seive.cpp

--- MATH/Number Theory/prime number/prime_precompute_seive.cpp
+++ MATH/Number Theory/prime number/prime_precompute_seive.cpp
@@ -1,12 +1,12 @@
 vector<ll> primes;
 
 void sieve(int limit) {
-    vector<bool> isPrime(limit + 1, true);
-    isPrime[0] = isPrime[1] = false;
-    for (int i = 2; i * i <= limit; ++i) {
-        if (isPrime[i]) {
-            for (int j = i * i; j <= limit; j += i) isPrime[j] = false;
+    vector<bool> is_prime(limit + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i <= limit; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= limit; j += i) is_prime[j] = false;
         }
     }
-    for (int i = 2; i <= limit; ++i) if (isPrime[i]) primes.push_back(i);
+    for (int i = 2; i <= limit; i++) if (is_prime[i]) primes.pb(i);
 }
